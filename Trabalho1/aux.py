769a94100a18de436315d1d635703225f688ffe4
Gilberto Cunha <gilbertocunha@MacBook-Pro-de-Gilberto.local>
2020-10-31 16:51:07 +0000

    comment the code

--- Trabalho1/aux.py
+++ Trabalho1/aux.py
@@ -45,26 +45,32 @@ class Project():
 
 def read_schedule(path):
     """
-    This function reads the csv file of a project's schedule
-    and transforms it into a pandas dataframe
+	Esta função lê o ficheiro csv de um projeto
+	e transforma-o num dataframe de pandas
     """
-    df = pd.read_csv(path)
+	
+    df = pd.read_csv(path) # Transformar csv em dataframe
     col_names = {}
+	# Coletar as horas presentes na primeira coluna do dataframe
     for i in range(len(df)):
         col_names[i] = df.iloc()[i][0]
-    df.drop(columns=[df.columns[0]], inplace=True)
-    df.rename(index=col_names, inplace=True)
+    df.drop(columns=[df.columns[0]], inplace=True) # Remover a primeira coluna do dataframe
+    df.rename(index=col_names, inplace=True) # Colocar as horas coletadas no nome das linhas do dataframe
     
     return df
 
 
 def get_project(path):
     """
-    This function reads a csv file of a project's schedule
-    and turns it into an object of the Project class
+	Esta função lê o ficheiro csv da disponibilidade
+	dos colaboradores de um projeto e transforma-o
+	num objeto da classe Projeto
     """
+	
+	# Converter ficheiro num dataframe pandas
     df = read_schedule(path)
     
+	# Colecionar os nomes dos colaboradores e líder
     names = []
     for col in df:
         for elem in df[col]:
@@ -73,26 +79,30 @@ def get_project(path):
                     names.append(name)
     names = list(dict.fromkeys(names))
 
+	# Criar dicionários para guardar as slots do líder e colaboradores
     leader = {name.split('*')[0]: [] for name in names if "*" in name}
     participants = {name: [] for name in names if "*" not in name}
 
+	# Popular os dicionários com os dados do dataframe
     for col in df:
         for i, elem in enumerate(df[col]):
             if elem != float:
-                # Append leader
+                # Append do líder
                 for l in leader:
                     if l in str(elem):
                         start, end = df.index[i].replace('h','').split('-')
                         leader[l].append((col.lower()[:3], int(start), int(end)))
-                # Append participants
+                # Append dos participantes
                 for p in participants:
                     if p in str(elem):
                         start, end = df.index[i].replace('h','').split('-')
                         participants[p].append((col.lower()[:3], int(start), int(end)))
 
+	# Transformar os dicionários em listas
     leader = next(iter(leader.values()))
     participants = [participants[key] for key in participants]
 
+	# Definir o id do projeto e o número de reuniões semanais
     project_id = path.split('/')[-1].split('.')[0]
     num_meets = int(input(f"How many meetings should be held for project {project_id} this week?"))
     
@@ -100,25 +110,39 @@ def get_project(path):
 
 def get_schedule(path, available_rooms):
     """
-    This function reads all project's csv files and turns them
-    into an object of the class Schedule
+	Esta função lê todos ficheiros csv na diretoria path
+	e converte-os num objeto da classe Schedule
     """
+	
     projects = []
+	# Colecionar os projetos de cada ficheiro csv numa lista
     for file in glob.glob(path + "/*.csv"):
         projects.append(get_project(file))
         
     return Schedule(projects, available_rooms)
 
 def pd_centered(df):
-    return df.style.set_table_styles([
-        {"selector": "th", "props": [("text-align", "center")]},
-        {"selector": "td", "props": [("text-align", "center")]}])
+	"""
+	Esta função centra as colunas de um dataframe de pandas
+	"""
+	return df.style.set_table_styles([
+		{"selector": "th", "props": [("text-align", "center")]},
+		{"selector": "td", "props": [("text-align", "center")]}])
 
 def meeting_to_df(r):
+	"""
+	Esta função converte uma lista de tuplos correspondentes às reuniões da semana
+	(correspondentes ao output do método get_meetings() da classe Schedule) num dataframe
+	de pandas com o horário completo
+	"""
+	
+	# Criar um dicionário com todos os dias e horas do horário
 	df_dict = {d: {f"{h}h-{h+1}h": "" for h in range(8, 17)} for d in ["mon","tue","wed","thu","fri"]}
-	for day in df_dict:
-		for hour in range(8, 17):
-			for name, d, h, room in r:
-				if day == d and hour == int(h):
-					df_dict[day][f"{hour}h-{hour+1}h"] += f"| {name} - {room} |"
+	
+	for day in df_dict: # Iterar os dias do dicionário
+		for hour in range(8, 17): # Iterar as horas do dicionário
+			for name, d, h, room in r: # Iterar os outputs do método get_mettings() 
+				if day == d and hour == int(h): # Verificar se o output corresponde ao dia e hora do horário
+					df_dict[day][f"{hour}h-{hour+1}h"] += f"| {name} - {room} |" # Adicionar o nome do projeto e a sala
+	
 	return pd.DataFrame(df_dict)
